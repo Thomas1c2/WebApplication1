ca3e0a775ad5f5a76f89b69855e1968acd58f173
Thomas Mezmur Birhanu <thomasmezmur1@gmail.com>
2021-03-25 09:38:54 +0300

    Adding OpenMap

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -6,4 +6,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  latitude: number = 8.9806;
+  longitude: number = 38.7578;
+
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([38.7578, 8.9806]),
+        zoom: 8
+      })
+    });
+  }
+
+  setCenter() {
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(8);
+  }
 }
+
+declare var ol: any;
